131843d0682ff9f8fc4d88fc8a16244ab9cbbbac
Jakub <42355410+kko7@users.noreply.github.com>
2020-03-27 01:00:42 +0100

    Create presence.py

---
+++ presence.py
@@ -0,0 +1,100 @@
+# Python libs
+import sys
+from time import gmtime, strftime, sleep
+import argparse
+import os
+
+# Custom libs
+import schedule
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options 
+
+# Argument parser configuration 
+parser = argparse.ArgumentParser(description="Gimme arguments")
+parser.add_argument("-u", "--username", help="Librus Synergia username", type=str, required=True)
+parser.add_argument("-p", "--password", help="Librus Synergia password", type=str, required=True)
+parser.add_argument("-hl", "--headless", help="Start in headless mode", action="store_true", required=False)
+parser.add_argument("-v", "--verbose", help="Start in verbose mode", action="store_true", required=False) # debugging mode
+args = parser.parse_args()
+
+# Print verbose messages
+def verbose(text):
+    if args.verbose:
+        print("[VERBOSE]" + text)
+
+class Presence():
+    def __init__(self):
+        self.driver = None
+        self.username = args.username
+        self.password = args.password
+        self.unread_messages = []
+
+    def setup(self):
+        options = Options()
+        if args.headless:
+            options.add_argument("--headless")
+            print("[Setup] Starting in headless mode")
+        else:
+            options = None
+            print("[Setup] Starting in normal mode")
+        self.driver = webdriver.Chrome(options=options)
+        print("[Setup] Finished")
+
+    def quit(self):
+        self.driver.quit()
+
+    def login(self):
+        self.driver.get("https://portal.librus.pl/rodzina")
+        self.driver.find_element(By.LINK_TEXT, "LIBRUS Synergia").click()
+        self.driver.find_element(By.LINK_TEXT, "Zaloguj").click()
+        self.driver.switch_to.frame(0)
+        print("[Login] Switched to login frame")
+        # Enter username
+        self.driver.find_element(By.ID, "Login").click()
+        self.driver.find_element(By.ID, "Login").send_keys(self.username)
+        print("[Login] Entered username")
+        # Enter password
+        self.driver.find_element(By.ID, "Pass").click()
+        self.driver.find_element(By.ID, "Pass").send_keys(self.password)
+        print("[Login] Entered password")
+        self.driver.find_element(By.ID, "LoginBtn").click()
+        sleep(2)
+        print("[Login] Finished")
+
+    def check_messages(self):
+        count = 0
+        self.driver.get("https://synergia.librus.pl/wiadomosci")
+        tbody = self.driver.find_elements(By.CSS_SELECTOR,
+         "#formWiadomosci > div > div > table > tbody > tr > td:nth-child(2) > table.decorated.stretch > tbody > tr"
+         )
+        for msg in tbody:
+            # Check if message is unread by checking style attribute
+            if "font-weight: bold;" in msg.find_element(By.CSS_SELECTOR, "td:nth-child(3)").get_attribute("style"):
+                # Add unread messages to array
+                self.unread_messages.append(msg.find_element(By.CSS_SELECTOR, "td:nth-child(4) > a").get_attribute("href"))
+                count += 1
+        print(f"[Check messages] Found {count} unread messages")
+        verbose("[Check messages] Unread messages links:\n" + "\n".join(map(str, self.unread_messages)))
+
+    def read_messages(self):
+        for index, message in enumerate(self.unread_messages):
+            self.driver.get(message) # Go to message link
+            self.unread_messages.pop(index) # Remove readed message from array
+            sleep(2)
+            # Save screenshot of message in homepath
+            self.driver.save_screenshot(os.path.join(os.environ['HOMEPATH'], f'wiadomosc_librus{strftime("%Y-%m-%d_%H-%M-%S")}.png'))
+
+if __name__ == "__main__":
+    presence = Presence()
+
+    try:
+        presence.setup()
+        presence.login()
+        schedule.every(1).minutes.do(lambda: presence.check_messages() or presence.read_messages())
+        while True:
+            schedule.run_pending()
+            sleep(1)
+
+    finally:
+        presence.quit()
